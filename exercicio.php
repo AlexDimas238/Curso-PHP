306cc375d80b6db45382f584491f207d945498d2
AlexDimas <alexdimas238@gmail.com>
2022-12-20 17:17:25 -0300

    first commit

---
+++ exercicio.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Sacramento&display=swap">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Exercício</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercício</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem Formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="principal">
+        <DIV class="conteudo">
+
+        </DIV>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS ₢ <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
